e0dc86cda21337430110e68220f451d7c4cf1869
caramelopardalis <ltsu.koiwai@gmail.com>
2020-09-25 20:02:49 +0900

    Support embedded font

--- src/from-dom.js
+++ src/from-dom.js
@@ -1,6 +1,10 @@
+import { name } from './ipag-normal.js';
 import { jsPDF } from 'jspdf';
 
+console.log(name);
+
 const doc = new jsPDF();
+doc.setFont('ipag', 'normal');
 doc.html(document.querySelector('.page'), {
     callback: function (doc) {
         doc.save();
