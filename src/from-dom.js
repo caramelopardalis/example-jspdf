e85962136766fd90b5798248c5647d6208f0bb42
caramelopardalis <ltsu.koiwai@gmail.com>
2020-09-30 16:01:17 +0900

    Implement A4 page fit layout

--- src/from-dom.js
+++ src/from-dom.js
@@ -3,7 +3,10 @@ import { jsPDF } from 'jspdf';
 
 console.log(name);
 
-const doc = new jsPDF();
+const doc = new jsPDF({
+    // DOM ツリーを px ベースでデザインしているので、px を指定する必要がある
+    unit: 'px'
+});
 doc.setFont('ipag', 'normal');
 doc.html(document.querySelector('.page'), {
     callback: function (doc) {
